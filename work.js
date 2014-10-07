66252304561bcd7ec197cfbf53c69a2568e4e7b8
Ken McLeod <admin@therinken.com>
2014-10-07 18:12:21 -0500

    startoff for hw

---
+++ work.js
@@ -0,0 +1,15 @@
+// Use EACH for the iteration methods discussed in class today (for loop, Array.forEach, 
+//	custom forEach) for EACH of the following problems...
+// 1. calculate the sum of numbers (can be any number of arguments)
+// 2. calculate the average of numbers
+// 3. find the largest number of the inputs
+// 4. find the longest string of the inputs
+// 5. given an array of Date() objects (Oct 4th 2014, Oct 3rd 2014, Sept 30th 2014, 
+//       Sept 1st 2012, March 13th 2010), in that order, sort them chronologically. 
+//          Use http://devdocs.io/javascript/global_objects/date for reference on creating 
+//             Date() objects with a specific date.
+
+function sum(a, b) {
+	return a + b;
+}
+sum(7, 6)
